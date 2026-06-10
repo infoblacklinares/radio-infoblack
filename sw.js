203fd4025fb106f35cf1a2500b12b60eaee1e425
willsondev <willsondev12@gmail.com>
2026-06-09 23:42:01 -0400

    PWA instalable + eliminacion persistente con localStorage + icono

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'infoblack-radio-v1';
+const ASSETS = ['/', '/index.html', '/icon.png', '/audio-1.mp3', '/audio-2.mp3', '/audio-3.mp3'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
